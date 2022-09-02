d9ff7e8fc2693bdba46908a59ee46f293c5a0c7f
rjhoskins <robertjhoskins@gmail.com>
2022-09-02 09:47:52 -0700

    rest of it

---
+++ prototype/ignore/NOTES.md
@@ -0,0 +1,15 @@
+## Bootstrap application
+
+`yarn create next-app trpc-tutorial-pre --typescript`
+
+## Install dependencies diff than CLI tool
+
+`npm i react-hook-form jsonwebtoken cookie nodemailer`
+
+`npm i @types/jsonwebtoken @types/cookie @types/nodemailer --save-dev`
+
+## Prisma
+
+`npx prisma init`
+
+`npx prisma migrate dev --name`
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,32 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
+
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../server/db/client";
+import { env } from "../../../env/server.mjs";
+
+export const authOptions: NextAuthOptions = {
+  // Include user.id on session
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID || '',
+      clientSecret: process.env.AUTH0_CLIENT_SECRET || '',
+      issuer: process.env.AUTH0_ISSUER || ''
+    })
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
